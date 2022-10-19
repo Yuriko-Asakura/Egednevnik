0005409b3a01e7fbff8c4680dbd4d28acfeb0ab6
Yriko-Asakura <isip_a.v.plahova@mpt.ru>
2022-10-19 19:22:20 +0300

    first commit

---
+++ Ежедневник/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ежедневник
+{
+    internal class dela
+    {
+        public string sob;
+        public string opis;
+        public string nomer;
+    }
+}
--- Ежедневник/Program.cs
+++ Ежедневник/Program.cs
@@ -1,52 +1,68 @@
 ﻿using System.Reflection.Metadata;
 using System.Security.Claims;
+using Ежедневник;
+
 namespace ежедневник
 {
     internal class Program
     {
         static void Main()
         {
-            int da = 1;
-            DateTime dates = new(2022, 10, da);
-            Console.WriteLine(dates + " нажмите Enter, что бы выбрать дату, " +
-                    "\n а потом нажмите на стрелку вверх или вниз что бы посмотрель заметку, \n" +
-                    "а чтобы увидеть более подробную информацию, снова нажмите Enter");
-            ConsoleKey date;
-            do
+            while (true)
             {
-                date = Console.ReadKey(true).Key;
-                Console.Clear();
-                if (date == ConsoleKey.LeftArrow)
+                int da = 1;
+                DateTime dates = new(2022, 10, da);
+                Console.WriteLine(dates + " нажмите Enter, что бы выбрать дату, " +
+                        "\n а потом нажмите на стрелку вверх или вниз что бы посмотрель заметку, \n" +
+                        "а чтобы увидеть более подробную информацию, снова нажмите Enter");
+                ConsoleKey date;
+                do
                 {
-                    da--;
-                    if (da < 1)
+                    date = Console.ReadKey(true).Key;
+                    Console.Clear();
+                    if (date == ConsoleKey.LeftArrow)
                     {
-                        da = 5;
+                        da--;
+                        if (da < 1)
+                        {
+                            da = 5;
+                        }
                     }
-                }
-                else if (date == ConsoleKey.RightArrow)
-                {
-                    da++;
-                    if (da > 5)
+                    else if (date == ConsoleKey.RightArrow)
                     {
-                        da = 1;
+                        da++;
+                        if (da > 5)
+                        {
+                            da = 1;
+                        }
                     }
+                    DateTime dats1 = new(2022, 10, da);
+                    Console.WriteLine(dats1 + " нажмите Enter, что бы выбрать дату, " +
+                        "\n а потом нажмите на стрелку вверх или вниз что бы посмотрель заметку, \n" +
+                        "а чтобы увидеть более подробную информацию, снова нажмите Enter");
                 }
-                DateTime dats1 = new(2022, 10, da);
-                Console.WriteLine(dats1 + " нажмите Enter, что бы выбрать дату, " +
-                    "\n а потом нажмите на стрелку вверх или вниз что бы посмотрель заметку, \n" +
-                    "а чтобы увидеть более подробную информацию, снова нажмите Enter");
-            }
-            while (date != ConsoleKey.Enter);
-            {
-                if (da == 1)
+                while (date != ConsoleKey.Enter);
                 {
-                    vabor();
-                }
-                if (da == 2)
-                {
-                    vabor2();
-
+                    if (da == 1)
+                    {
+                        vabor();
+                    }
+                    if (da == 2)
+                    {
+                        vabor2();
+                    }
+                    if (da == 3)
+                    {
+                        vabor3();
+                    }
+                    if (da == 4)
+                    {
+                        vabor4();
+                    }
+                    if (da == 5)
+                    {
+                        vabor5();
+                    }
                 }
             }
 
@@ -87,53 +103,251 @@ namespace ежедневник
         static void vabor()
         {
             strelka();
-            string[] num = new string[] { "Полить цветы", "Сходить в библиотеку", "Сделать домашку" };
             if (posit == 1)
             {
                 Console.Clear();
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Полить цветы";
+                opis1.opis = "Именно: незабудки, орхидею и розы";
+                nomer1.nomer = "В первый день Октября";
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(num[0]);
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
                 Console.ForegroundColor = ConsoleColor.White;
             }
             else
             if (posit == 2)
             {
-                Console.Clear();
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Выгулить собаку";
+                opis1.opis = "Не идти в парк днём, а пойти в лес";
+                nomer1.nomer = "В первый день Октября";
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(num[1]);
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
                 Console.ForegroundColor = ConsoleColor.White;
             }
             else
             if (posit == 3)
             {
-                Console.Clear();
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Сделать домашнее задание";
+                opis1.opis = "По дисциплинам: C#, Pyuton и Pascal";
+                nomer1.nomer = "В первый день Октября";
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(num[2]);
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
                 Console.ForegroundColor = ConsoleColor.White;
             }
-            Main();
 
         }
         static void vabor2()
         {
             strelka();
-            string[] num = new string[] { "Выгулить собаку", "Посмотреть фильм -Веном- ", "" };
             if (posit == 1)
             {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Сделать домашнее задание";
+                opis1.opis = "По дисциплинам: C#, Pyuton";
+                nomer1.nomer = "2 день Октября";
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(num[0]);
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
                 Console.ForegroundColor = ConsoleColor.White;
             }
             else
             if (posit == 2)
             {
-                Console.Clear();
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Сделать домашнее задание";
+                opis1.opis = "По дисциплинам: Pascal";
+                nomer1.nomer = "2 день Октября";
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(num[1]);
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
                 Console.ForegroundColor = ConsoleColor.White;
             }
-            Main();
+            else
+            if (posit == 3)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Ничего не запланировано на это время";
+                opis1.opis = "Свобода";
+                nomer1.nomer = "2 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
 
         }
+        static void vabor3()
+        {
+            strelka();
+            if (posit == 1)
+            {
+                Console.Clear();
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Сходить в библиотеку";
+                opis1.opis = "Взять книги Война и мир, Лолита и Русалочка";
+                nomer1.nomer = "3 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            if (posit == 2)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Выгулить собаку";
+                opis1.opis = "Не идти в парк днём,";
+                nomer1.nomer = "3 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            if (posit == 3)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Сделать домашнее задание";
+                opis1.opis = "По дисциплинам: Философия и литература";
+                nomer1.nomer = "3 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+        static void vabor4()
+        {
+            strelka();
+            if (posit == 1)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Выполнить заказ";
+                opis1.opis = "Нарисовать портртет девушки и парня, дедлайн через неделю";
+                nomer1.nomer = "4 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            if (posit == 2)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "На это время ничего не запланировано";
+                opis1.opis = "Свобода";
+                nomer1.nomer = "4 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            if (posit == 3)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Заказ";
+                opis1.opis = "Забрать посылку из почты";
+                nomer1.nomer = "4 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+        static void vabor5()
+        {
+            strelka();
+            if (posit == 1)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Ничего не запланировано на это время";
+                opis1.opis = "Свобода";
+                nomer1.nomer = "5 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            if (posit == 2)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Сходить в магазин";
+                opis1.opis = "Купить: Яйца, бекон, макороны,  сыр и помидоры";
+                nomer1.nomer = "5 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            if (posit == 3)
+            {
+                dela sob1 = new dela();
+                dela opis1 = new dela();
+                dela nomer1 = new dela();
+                sob1.sob = "Ничего не запланировано на это время";
+                opis1.opis = "Свобода";
+                nomer1.nomer = "5 день Октября";
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(sob1.sob);
+                Console.WriteLine(opis1.opis);
+                Console.WriteLine(nomer1.nomer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+        }
+
+
     }
 }
